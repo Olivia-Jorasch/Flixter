1461789f71e38ac73bee010bb516af1b5e51a79c
Olivia Jorasch <ojorasch@fb.com>
2018-06-28 15:22:53 -0700

    added collection view

---
+++ flixter/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  flixter
+//
+//  Created by Olivia Jorasch on 6/28/18.
+//  Copyright © 2018 FBU. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
---
+++ flixter/View Controllers/GridViewController.h
@@ -0,0 +1,13 @@
+//
+//  GridViewController.h
+//  flixter
+//
+//  Created by Olivia Jorasch on 6/28/18.
+//  Copyright © 2018 FBU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GridViewController : UIViewController
+
+@end
